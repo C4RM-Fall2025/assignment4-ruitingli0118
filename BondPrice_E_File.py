741b29f05e101ec2b408f7fbb420bbe7c8a69910
ruitingli0118 <rl3518@columbia.edu>
2025-10-05 14:52:59 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,17 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    coupon = face * couponRate
+    bondPrice = 0
+
+    # enumerate starts at index 0, so add 1 to get the time period t
+    for t, y in enumerate(yc, start=1):
+        if t < m:
+            cf = coupon
+        else:
+            # Last period gets coupon + face
+            cf = coupon + face
+        pv_factor = 1 / ((1 + y) ** t)
+        bondPrice += cf * pv_factor
+
+    return bondPrice
